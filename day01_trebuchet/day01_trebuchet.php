40a2c08e4d7923f91f2c2193f82a9dc816f8ee4c
Cor Bosman <cor@xs4all.net>
2023-12-01 21:29:49 +0100

    wip

--- day01_trebuchet/day01_trebuchet.php
+++ day01_trebuchet/day01_trebuchet.php
@@ -4,21 +4,21 @@ use Tightenco\Collect\Support\Collection;
 
 class day01_trebuchet extends solver
 {
+    private array $digits = ['one' => 1, 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5, 'six' => 6, 'seven' => '7', 'eight' => 8, 'nine' => 9];
+
     public function solve() : array
     {
         $this->start_timer();
 
-        $digits = ['one' => 1, 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5, 'six' => 6, 'seven' => '7', 'eight' => 8, 'nine' => 9];
-
         $sum = $this->input->map(fn($l) => preg_replace("~\D~", '', $l))->map(fn($c) => (int)$c[0] . $c[-1])->sum();
         $this->solution('1a', $sum);
 
-        $sum = $this->input->map(function($line) use ($digits) {
+        $sum = $this->input->map(function($line) {
             preg_match('/^.*(one|two|three|four|five|six|seven|eight|nine|\d)/U', $line, $matches);
-            $first = is_numeric($matches[1]) ? $matches[1] : $digits[$matches[1]];
+            $first = is_numeric($matches[1]) ? $matches[1] : $this->digits[$matches[1]];
 
             preg_match('/^.*(one|two|three|four|five|six|seven|eight|nine|\d).*$/', $line, $matches);
-            $last = is_numeric($matches[1]) ? $matches[1] : $digits[$matches[1]];
+            $last = is_numeric($matches[1]) ? $matches[1] : $this->digits[$matches[1]];
 
             return (int)"{$first}{$last}";
         })->sum();
