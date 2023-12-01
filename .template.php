86cdd85bef9e6bfe31c486ed73098a0c43f539cd
Cor Bosman <cor@xs4all.net>
2023-12-01 15:55:58 +0100

    wip

---
+++ .template.php
@@ -0,0 +1,13 @@
+<?php namespace XXX;
+use Lib\solver;
+use Tightenco\Collect\Support\Collection;
+
+class XXX extends solver
+{
+    public function solve() : array
+    {
+        $this->start_timer();
+
+        return $this->solutions;
+    }
+ }
