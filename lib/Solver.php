58c7e1029eec8301c9217c162252ec206dec723f
Cor Bosman <cor@xs4all.net>
2023-12-01 15:55:39 +0100

    wip

---
+++ lib/Solver.php
@@ -0,0 +1,57 @@
+<?php namespace Lib;
+use ReflectionClass;
+use Tightenco\Collect\Support\Collection;
+
+class Solver
+{
+    protected float $timer;
+    protected array $solutions = [];
+    protected Collection $input;
+    protected ReflectionClass $reflection;
+
+    public function __construct($options)
+    {
+        $this->reflection = new ReflectionClass($this);
+        $puzzle = $options['e'] ?? $this->puzzle();
+        $this->input = $this->get_input($puzzle);
+    }
+
+    public function start_timer() : void
+    {
+        $this->timer = microtime(true);
+    }
+
+    /* add a solution */
+    public function solution($puzzle, $value) : void
+    {
+        $time = microtime(true);
+        $this->solutions[] = [$puzzle, $this->title(), $value, $time - $this->timer];
+        $this->timer = $time;
+    }
+
+    /* get the puzzle number from the class name */
+    public function puzzle() : string
+    {
+        return substr($this->reflection->getShortName(), 3, 2);
+    }
+
+    /* get the title from the class name */
+    public function title() : string
+    {
+        return ucwords(str_replace('_', ' ', substr($this->reflection->getShortName(), 6)));
+    }
+
+    private function get_input(mixed $puzzle) : Collection
+    {
+        $filename = dirname($this->reflection->getFileName()) . "/input/{$puzzle}.txt";
+        if (file_exists($filename)) {
+            return $this->read_input($filename);
+        }
+        die("no input for puzzle {$puzzle}");
+    }
+
+    public function read_input($filename) : mixed
+    {
+        return collect(file($filename, FILE_IGNORE_NEW_LINES));
+    }
+}
---
+++ lib/helpers.php
@@ -0,0 +1,77 @@
+<?php
+use function Termwind\{render};
+
+function output($str) : void
+{
+    print_r($str);
+    print_r("\n");
+}
+
+function render_output($solutions) : void
+{
+    $total_time = 0;
+    $table = <<<HTML
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th class='text-center'>PUZZLE</th>
+                    <th class='text-center'>TITLE</th>
+                    <th class='text-center'>ANSWER</th>
+                    <th class='text-center'>RUNTIME</th>
+                </tr>
+            </thead>
+    HTML;
+
+    foreach($solutions->all() as $solution) {
+        [$puzzle, $title, $value, $time] = $solution;
+        $total_time += $time;
+        $time = round(($time)*1000,3);
+
+        $table .= <<<HTML
+        <tr class='text-center'>
+          <td class='text-center text-yellow-400'>{$puzzle}</td>
+          <td class='text-center text-yellow-400'>{$title}</td>
+          <td class='text-center text-green-400'>{$value}</td>
+          <td class='text-center text-blue-400'>{$time} ms</td>
+        </tr>
+        HTML;
+    }
+
+    $total_time = round(($total_time)*1000,3);
+
+    $table .= <<<HTML
+        <tr>
+                          <td class='text-center text-yellow-400'></td>
+                        <td class='text-center text-yellow-400'></td>
+                        <td class='text-center text-green-400'></td>
+                        <td class='text-center text-blue-400'>---------</td>
+        </tr>
+        <tr>
+                          <td class='text-center text-yellow-400'></td>
+                        <td class='text-center text-yellow-400'></td>
+                        <td class='text-center text-green-400'></td>
+                        <td class='text-center text-blue-400'>{$total_time} ms</td>
+        </tr>
+        </table>
+    </div>
+    HTML;
+
+    render($table);
+
+}
+
+function print_grid(array $grid, $w)
+{
+    $width = count($grid[0]);
+    $height = count($grid);
+
+    for($i=0; $i<$height; $i++) {
+        for($j=0; $j<$width; $j++) {
+            echo sprintf("%{$w}s ", $grid[$i][$j]);
+        }
+        echo "\n";
+    }
+    echo "\n";
+}
+
