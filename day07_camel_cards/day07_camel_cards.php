ad32478f7a2ca704d55560da694ca64fe82ed6ff
Cor Bosman <cor@xs4all.net>
2023-12-07 11:58:00 +0100

    wip

---
+++ day07_camel_cards/day07_camel_cards.php
@@ -0,0 +1,11 @@
+<?php namespace day07_camel_cards;
+use Lib\solver;
+use Tightenco\Collect\Support\Collection;
+
+class day07_camel_cards extends solver
+{
+    public function solve() : void
+    {
+        //
+    }
+ }
