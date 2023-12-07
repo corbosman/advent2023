5316a93d49cab60ea3fd7302a27bbd8cd893029f
Cor Bosman <cor@xs4all.net>
2023-12-07 17:02:26 +0100

    wip

--- day07_camel_cards/Hand.php
+++ day07_camel_cards/Hand.php
@@ -87,3 +87,14 @@ class Hand
         return hexdec(implode('', array_map(fn($card) =>  $this->scores[$card] ?? $card, $cards)));
     }
 }
+
+enum Type : int
+{
+    case FIVE_OF_A_KIND = 7;
+    case FOUR_OF_A_KIND = 6;
+    case FULL_HOUSE = 5;
+    case THREE_OF_A_KIND = 4;
+    case TWO_PAIRS = 3;
+    case ONE_PAIR = 2;
+    case HIGH_CARD = 1;
+}
