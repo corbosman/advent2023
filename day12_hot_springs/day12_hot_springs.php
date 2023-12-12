1ead6a0145c1f0cff267f29aed5f14fa19f58122
Cor Bosman <cor@xs4all.net>
2023-12-12 11:23:00 +0100

    wip

---
+++ day12_hot_springs/day12_hot_springs.php
@@ -0,0 +1,11 @@
+<?php namespace day12_hot_springs;
+use Lib\solver;
+use Tightenco\Collect\Support\Collection;
+
+class day12_hot_springs extends solver
+{
+    public function solve() : void
+    {
+        //
+    }
+ }
