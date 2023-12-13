646efbb53fa5c16de05dd36c0b40477b97f75263
Cor Bosman <cor@xs4all.net>
2023-12-13 15:23:31 +0100

    wip

---
+++ day13_point_of_incidence/day13_point_of_incidence.php
@@ -0,0 +1,11 @@
+<?php namespace day13_point_of_incidence;
+use Lib\solver;
+use Tightenco\Collect\Support\Collection;
+
+class day13_point_of_incidence extends solver
+{
+    public function solve() : void
+    {
+        //
+    }
+ }
