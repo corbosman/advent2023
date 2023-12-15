5b69f9f4193821b6b47f64b21842da77b0ce8481
Cor Bosman <cor@xs4all.net>
2023-12-15 09:22:27 +0100

    wip

---
+++ day15_lens_library/day15_lens_library.php
@@ -0,0 +1,11 @@
+<?php namespace day15_lens_library;
+use Lib\solver;
+use Illuminate\Support\Collection;
+
+class day15_lens_library extends solver
+{
+    public function solve() : void
+    {
+        //
+    }
+ }
