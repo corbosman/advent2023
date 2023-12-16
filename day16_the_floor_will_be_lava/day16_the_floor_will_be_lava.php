5af47cfd3b0dad18414cfb65c23dcf1e96d920f0
Cor Bosman <cor@xs4all.net>
2023-12-16 11:22:26 +0100

    wip

---
+++ day16_the_floor_will_be_lava/day16_the_floor_will_be_lava.php
@@ -0,0 +1,11 @@
+<?php namespace day16_the_floor_will_be_lava;
+use Lib\solver;
+use Illuminate\Support\Collection;
+
+class day16_the_floor_will_be_lava extends solver
+{
+    public function solve() : void
+    {
+        //
+    }
+ }
