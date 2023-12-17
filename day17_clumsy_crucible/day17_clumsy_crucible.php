4c699e246ad021165d48df173bc39f8423e1c552
Cor Bosman <cor@xs4all.net>
2023-12-17 10:34:27 +0100

    wip

---
+++ day17_clumsy_crucible/day17_clumsy_crucible.php
@@ -0,0 +1,11 @@
+<?php namespace day17_clumsy_crucible;
+use Lib\solver;
+use Illuminate\Support\Collection;
+
+class day17_clumsy_crucible extends solver
+{
+    public function solve() : void
+    {
+        //
+    }
+ }
