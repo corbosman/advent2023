3df45002a322950ade0d6d773bcabefbe6febf2f
Cor Bosman <cor@xs4all.net>
2023-12-18 08:41:57 +0100

    wip

---
+++ day18_lavaduct_lagoon/day18_lavaduct_lagoon.php
@@ -0,0 +1,11 @@
+<?php namespace day18_lavaduct_lagoon;
+use Lib\solver;
+use Illuminate\Support\Collection;
+
+class day18_lavaduct_lagoon extends solver
+{
+    public function solve() : void
+    {
+        //
+    }
+ }
