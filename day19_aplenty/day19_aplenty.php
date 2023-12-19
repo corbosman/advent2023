65b84b01c1ed9611928746d896c46716252049c4
Cor Bosman <cor@xs4all.net>
2023-12-19 14:08:32 +0100

    wip

---
+++ day19_aplenty/day19_aplenty.php
@@ -0,0 +1,11 @@
+<?php namespace day19_aplenty;
+use Lib\solver;
+use Illuminate\Support\Collection;
+
+class day19_aplenty extends solver
+{
+    public function solve() : void
+    {
+        //
+    }
+ }
