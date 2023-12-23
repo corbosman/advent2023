b150fe8b02113deb5179cb16f5f9da015f818672
Cor Bosman <cor@xs4all.net>
2023-12-23 11:37:36 +0100

    wip

---
+++ day20_pulse_propagation/day20_pulse_propagation.php
@@ -0,0 +1,11 @@
+<?php namespace day20_pulse_propagation;
+use Lib\solver;
+use Illuminate\Support\Collection;
+
+class day20_pulse_propagation extends solver
+{
+    public function solve() : void
+    {
+        //
+    }
+ }
