dece2f3487d540f713e33de97c297eb9facef17f
Cor Bosman <cor@xs4all.net>
2023-12-23 19:02:13 +0100

    wip

---
+++ day21_step_counter/day21_step_counter.php
@@ -0,0 +1,11 @@
+<?php namespace day21_step_counter;
+use Lib\solver;
+use Illuminate\Support\Collection;
+
+class day21_step_counter extends solver
+{
+    public function solve() : void
+    {
+        //
+    }
+ }
